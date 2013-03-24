ba0f993509e321fd1cd1758dc8ff4e6fb4390c3f
David Mills <admin@davermills.com>
2013-03-24 16:01:53 +0000

    Initial commint

---
+++ Playground.Model/Interfaces/IMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Playground.Model.Interfaces
+{
+    interface IMenuItem
+    {
+        public string Reference { get; set; }
+        public string Title { get; set; }
+        public List<IMenuItem> SubItems { get; set; }
+    }
+}
