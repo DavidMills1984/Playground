ad488084a2325a8454c686ef613b961c753ffec1
David Mills <admin@davermills.com>
2013-05-19 14:35:53 +0100

    Added hero image rotation (Fade in/out)

--- Playground/Scripts/Site/UI.js
+++ Playground/Scripts/Site/UI.js
@@ -21,18 +21,52 @@
     );
 
     $(".tooltip").hover(function () {
-        var tooltipText = $(this).attr("tooltip-text");
-        $(this).parent().append("<div style='opacity:0;' class='tooltip-wrapper'><div class='tooltip-inner'><p>" + tooltipText + "</p></div></div>");
-        $(this).siblings(".tooltip-wrapper").animate({ "opacity": "1" }, "slow");
+        if ($(this).siblings(".tooltip-wrapper").length == 0) {
+            var tooltipText = $(this).attr("tooltip-text");
+            $(this).parent().append("<div style='opacity:0;' class='tooltip-wrapper'><div class='tooltip-inner'><p>" + tooltipText + "</p></div></div>");
+            $(this).siblings(".tooltip-wrapper").animate({ "opacity": "1" }, "slow");
+        }
+
     },
     function () {
         $(this).siblings(".tooltip-wrapper").animate({
-            opacity: '0'}, {
+            opacity: '0'
+        }, {
             duration: "slow",
             complete: function () {
-                $(this).remove(); 
+                $(this).remove();
             }
         })
     }
     );
+
+    $(".hs-img").HeroSwitching();
+
 });
+
+$.fn.HeroSwitching = function () {
+
+    var imageAmount = $(this).attr("hs-img-amt");
+    HeroSwitch($(this), 1, imageAmount);
+}
+
+function HeroSwitch($image, currentImage, imageAmount) {
+
+    //Amount of time taken to fade a Hero image in and out
+    var fadeDelay = 2500;
+    //Amount of time a Hero image is displayed for
+    var switchDelay = 15000
+
+    $image.delay(switchDelay).fadeToggle(fadeDelay, function () {
+        currentImage++;
+        if (currentImage > imageAmount) {
+            currentImage = 1;
+        }
+        //Switch the hero image using css
+        $image.css("background-image", "url('/Images/HeroScroller/hero-image-" + currentImage + ".png')");
+        //Fade hero image back in then call this function to loop back around the process
+        $image.fadeToggle(fadeDelay, function () { HeroSwitch($image, currentImage, imageAmount); });
+    });
+
+        
+}
